1d012373816afd9ca566908ecf079b46d58ee5ed
Thiago Dantas <thiago.dantas@stone.com.br>
2022-10-10 16:46:11 -0300

    test: add more tests of app.js file components

--- src/App.test.js
+++ src/App.test.js
@@ -4,9 +4,23 @@ import { render, screen } from "@testing-library/react";
 import App from "./app";
 
 describe("Componente principal", () => {
-  it("Mostrar o nome do banco", () => {
-    render(<App />);
+  describe("Quando eu abro o app do banco", () => {
+    test("o nome é exibido", () => {
+      render(<App />);
 
-    expect(screen.getByText("ByteBank")).toBeInTheDocument();
+      expect(screen.getByText("ByteBank")).toBeInTheDocument();
+    });
+
+    test("o saldo é exibido", () => {
+      render(<App />);
+
+      expect(screen.getByText("Saldo:")).toBeInTheDocument();
+    });
+
+    test("o botão de realizar transação é exibido", () => {
+      render(<App />);
+
+      expect(screen.getByText("Realizar operação")).toBeInTheDocument();
+    });
   });
 });
